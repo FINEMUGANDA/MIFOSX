e0b64be5b5535cae8897c1b020ab3a29d1eafaa1
Aleksandar Vidakovic <cheetah@monkeysintown.com>
2015-04-15 12:08:53 +0100

    Add missing client status code values

---
+++ mifosng-provider/src/main/resources/sql/migrations/core_db/V246__add_missing_client_status_code_values.sql
@@ -0,0 +1,2 @@
+INSERT INTO `mifostenant-default`.m_code_value (code_id, code_value, code_description, order_position, code_score) VALUES (26, 'others', null, 1, null);
+INSERT INTO `mifostenant-default`.m_code_value (code_id, code_value, code_description, order_position, code_score) VALUES (27, 'others', null, 1, null);
