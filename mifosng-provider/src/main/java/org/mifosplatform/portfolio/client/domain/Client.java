d0394eac086a5aa4f5509fd47bd9f9a2d7fcf80d
Aleksandar Vidakovic <cheetah@monkeysintown.com>
2015-08-19 12:44:22 +0100

    Trigger build

--- mifosng-provider/src/main/java/org/mifosplatform/portfolio/client/domain/Client.java
+++ mifosng-provider/src/main/java/org/mifosplatform/portfolio/client/domain/Client.java
@@ -366,6 +366,7 @@ public final class Client extends AbstractPersistable<Long> {
     public void updateAccountNo(final String accountIdentifier) {
         this.accountNumber = accountIdentifier;
         this.accountNumberRequiresAutoGeneration = false;
+
         if(StringUtils.isEmpty(this.externalId) && !StringUtils.isEmpty(accountIdentifier) && accountIdentifier.length()>=5) {
             this.externalId = accountIdentifier.substring(accountIdentifier.length()-5, accountIdentifier.length());
         }
