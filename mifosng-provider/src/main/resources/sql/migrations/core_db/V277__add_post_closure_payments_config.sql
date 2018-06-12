b7512618f26916f5383ded9725cf27cf6856f491
Frank Nkuyahaga <frank@Franks-MacBook-Pro.local>
2018-06-12 09:57:37 +0300

    Refs #132: Add post loan closure repayment feature

---
+++ mifosng-provider/src/main/resources/sql/migrations/core_db/V277__add_post_closure_payments_config.sql
@@ -0,0 +1,2 @@
+INSERT INTO `c_configuration` (`name`, `value`, `enabled`, `description`)
+VALUES ('allow-post-closure-repayments', 1, 1, 'Allows repayments to be made to closed loans in order to make the refund-to-client function available.');
